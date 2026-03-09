c8b8e9baa3c94f40ab33f3a47a12c89f95582804
mayatuncodes <mayatuncodes@gmail.com>
2026-03-09 18:07:44 +0600

    Modify sign js file

--- script/signin.js
+++ script/signin.js
@@ -12,22 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const password = passwordInput.value;
 
     if (username === "admin" && password === "admin123") {
-      successMsg.classList.remove("hidden");
-
-      setTimeout(() => {
-        window.location.href = "dashboard.html";
-      }, 1500);
+      alert("Login Successful");
+      window.location.href = "dashboard.html";
     } else {
       alert("Login Failed");
     }
   });
 
-  // Enter press anywhere in form fills auto credentials
-  form.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-      e.preventDefault(); // Prevent default submit
-      usernameInput.value = "admin";
-      passwordInput.value = "admin123";
-    }
-  });
 });
